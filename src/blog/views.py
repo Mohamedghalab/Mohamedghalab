a9ace9eff6ca2d2b38f6cd39ae015c344b47bd96
mohamed ghalab <Mohamedghalab@localhost.localdomain>
2020-03-29 21:29:50 +0200

    base.html about.html fontawsem facebook , route

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,6 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+posts = [
+    {
+        'title':'التدوينه الاولى',
+        'content':'هذا نص تجريبى خاص بالتدوينه الاولى',
+        'author':'Mohamed ghalab',
+        'date_posted':'29-3-2020'
+    },
+    {
+        'title':'التدوينه الثانيه',
+        'content':'هذا نص تجريبى خاص بالتدوينه الثانيه',
+        'author':'Mohamed ghalab',
+        'date_posted':'29-3-2020'
+    },
+    {
+        'title':'التدوينه الثالثه',
+        'content':'هذا نص تجريبى خاص بالتدوينه الثالثه',
+        'author':'Mohamed ghalab',
+        'date_posted':'29-3-2020'
+    },
+    {
+        'title':'التدوينه الرابعه',
+        'content':'هذا نص تجريبى خاص بالتدوينه الرابعه',
+        'author':'Mohamed ghalab',
+        'date_posted':'29-3-2020'
+    }
+]
 
 def home(request):
-    return render(request,'blog/index.html',{})
+    context = {
+        'title':'الصفحة الرئيسيه',
+        'posts':posts,
+    }
+    return render(request,'blog/index.html',context)
+
+def about(request):
+    return render(request, 'blog/about.html')
